bb65fbabd583003080b1bb6174aa4656960bdb18
Minh's Dell <minhnle311@gmail.com>
2024-01-21 03:17:44 -0800

    stuff

--- Client/wwwroot/js/dialog.js
+++ Client/wwwroot/js/dialog.js
@@ -1,31 +1,31 @@
-﻿window.ShowDialog = () => {
+﻿window.ShowDialog = function() {
     document.getElementById('my-dialog').showModal()
 }
 
-let currentIndex = 0;
+//let currentIndex = 0;
 
-const showSlides = () => {
-    const carousel = document.querySelector('.carousel')
-    const totalItems = document.querySelectorAll('.carousel-item').length
+//const showSlides = () => {
+//    const carousel = document.querySelector('.carousel')
+//    const totalItems = document.querySelectorAll('.carousel-item').length
 
-    if (currentIndex < 0) {
-        currentIndex = totalItems - 3;
-    } else if (currentIndex >= totalItems - 2) {
-        currentIndex = 0
-    }
+//    if (currentIndex < 0) {
+//        currentIndex = totalItems - 3;
+//    } else if (currentIndex >= totalItems - 2) {
+//        currentIndex = 0
+//    }
 
-    const translateValue = (-currentIndex * (100 / 3)).toString() + '%'
-    carousel.style.transform = 'translateX(' + translateValue + ')'
-}
+//    const translateValue = (-currentIndex * (100 / 3)).toString() + '%'
+//    carousel.style.transform = 'translateX(' + translateValue + ')'
+//}
 
-const prevSlide = () => {
-    currentIndex--
-    showSlides()
-}
+//const prevSlide = () => {
+//    currentIndex--
+//    showSlides()
+//}
 
-const nextSlide = () => {
-    currentIndex++
-    showSlides()
-}
+//const nextSlide = () => {
+//    currentIndex++
+//    showSlides()
+//}
 
-showSlides()
+//showSlides()
